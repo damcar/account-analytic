771602a7072b5b104233cdbc565484594f7b92dd
cubells <vicent@vcubells.net>
2018-07-12 15:29:29 +0200

    [MIG] account_analytic_distribution: Migration to 11.0

--- account_analytic_distribution/models/res_company.py
+++ account_analytic_distribution/models/res_company.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Vicent Cubells - <vicent.cubells@tecnativa.com>
+# Copyright 2017-2018 Vicent Cubells - <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
--- account_analytic_distribution/tests/test_analytic_distribution.py
+++ account_analytic_distribution/tests/test_analytic_distribution.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 - Tecnativa - Vicent Cubells
+# Copyright 2017-2018 - Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests import common
